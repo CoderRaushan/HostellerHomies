688f14dade1929ff04dde9589d35c98a554780a6
jay kumar <kumarprince13833@gmail.com>
2025-06-17 22:13:35 +0530

    bug

--- backend/controllers/invoiceController.js
+++ backend/controllers/invoiceController.js
@@ -12,8 +12,11 @@ exports.generateInvoices = async (req, res) => {
         return res.status(400).json({ errors: errors.array(), success });
     }
     const { hostel } = req.body;
-    const students = await Student.find({ hostel })
-    const invoices = await Invoice.find({ student: { $in: students }, date: { $gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1) } })
+    const students = await Student.find({ hostel });
+    const invoices = await Invoice.find({
+        student: { $in: students },
+        date: { $gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1) }
+    });
     if (invoices.length === students.length) {
         return res.status(400).json({ errors: 'Invoices already generated', success });
     }
@@ -21,38 +24,35 @@ exports.generateInvoices = async (req, res) => {
     // get days in previous month
     let daysinlastmonth = new Date(new Date().getFullYear(), new Date().getMonth(), 0).getDate();
 
-    let amount = Mess_bill_per_day * daysinlastmonth;
-    count = 0;
-    students.map(async (student) => {
-        let messoff = await MessOff.find({ student: student });
-        if (messoff) {
-            messoff.map((messoffone) => {
-                if (messoffone.status === 'approved' && messoffone.return_date.getMonth() + 1 === new Date().getMonth()) {
-                    let leaving_date = messoffone.leaving_date;
-                    let return_date = messoffone.return_date;
-                    let number_of_days = (return_date - leaving_date) / (1000 * 60 * 60 * 24);
-                    amount -= Mess_bill_per_day * number_of_days;
-                }
-            });
-        }
-
-        try {
+    let count = 0;
+    try {
+        await Promise.all(students.map(async (student) => {
+            let amount = Mess_bill_per_day * daysinlastmonth;
+            let messoff = await MessOff.find({ student: student });
+            if (messoff) {
+                messoff.forEach((messoffone) => {
+                    if (messoffone.status === 'approved' && messoffone.return_date.getMonth() + 1 === new Date().getMonth()) {
+                        let leaving_date = messoffone.leaving_date;
+                        let return_date = messoffone.return_date;
+                        let number_of_days = (return_date - leaving_date) / (1000 * 60 * 60 * 24);
+                        amount -= Mess_bill_per_day * number_of_days;
+                    }
+                });
+            }
             let invoice = new Invoice({
                 student,
                 amount
             });
             await invoice.save();
             count++;
-        }
-        catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server error');
-        }
-    });
-    success = true;
-    res.status(200).json({ success, count });
-}
-
+        }));
+        success = true;
+        res.status(200).json({ success, count });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
 // @route   GET api/invoice/getbyid
 // @desc    Get all invoices
 // @access  Public
